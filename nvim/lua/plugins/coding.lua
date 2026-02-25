b26a1bd147b69586e4c5874e00421966ef215cf4
anekobtw <79448362+anekobtw@users.noreply.github.com>
2026-02-24 22:55:15 -0500

    Made the plugins files more neat

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,18 +1,6 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-	},
-	{
-		"windwp/nvim-ts-autotag",
-	},
+	{ "nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate" },
+	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+	{ "lewis6991/gitsigns.nvim" },
+	{ "windwp/nvim-ts-autotag" },
 }
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,36 +1,12 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1001
-  },
-	{
-		"nvim-tree/nvim-web-devicons",
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		lazy = false,
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	},
-	{
-		"folke/todo-comments.nvim",
-	},
-	{
-		"akinsho/bufferline.nvim",
-	},
-	{
-		"numToStr/Comment.nvim",
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-	},
-	{
-		"folke/which-key.nvim",
-	},
-	{
-		"luxvim/nvim-luxterm",
-	},
-  {
-    "sethen/line-number-change-mode.nvim",
-  }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "nvim-lualine/lualine.nvim", lazy = false, requires = { "nvim-tree/nvim-web-devicons", opt = true } },
+	{ "folke/todo-comments.nvim" },
+	{ "akinsho/bufferline.nvim" },
+	{ "numToStr/Comment.nvim" },
+	{ "nvim-tree/nvim-tree.lua" },
+	{ "folke/which-key.nvim" },
+	{ "luxvim/nvim-luxterm" },
+	{ "sethen/line-number-change-mode.nvim" },
 }
